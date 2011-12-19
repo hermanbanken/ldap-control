2c2d0ef0e23edf2d428f3852f07ff070025b2c63
Herman Banken <hermanbanken@gmail.com>
2011-12-19 14:13:35 +0100

    Glossy overlay for user icons

--- login.php
+++ login.php
@@ -3,7 +3,8 @@
 		foreach($l->get_users() as $user){
 			if(isset($user->jpegphoto)){
 			?><li class="avatar" uid="<?php echo $user->uid; ?>">
-				<div>
+				<div class='jpegPhoto'>
+					<div class='glossy'></div>
 					<img src="data:image/jpeg;base64,<?php echo base64_encode($user->jpegphoto); ?>" />
 				</div>
 				<span class='uid'><?php echo $user->cn; ?></span>
